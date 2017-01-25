0cb75bc4c044ea2948a573289b85f38efafb9555
Somin Wadhwa <sominwadhwa@gmail.com>
2017-01-26 04:30:41 +0530

    Fixed insertion referencing

--- Stacks&Queues/QBasic.py
+++ Stacks&Queues/QBasic.py
@@ -10,4 +10,36 @@ class Queue(object):
         self.first = None
         self.last = None
     def insert(self,data):
+        node = Node(data)
+        if (self.last != None):
+            self.last.next = node
+        self.last = node
+        if (self.first == None):
+            self.first = self.last
+    def remove(self):
+        if (self.first == None):
+            return ("Underflow")
+        else:
+            item = self.first.data
+            self.first = self.first.next
+            return item
+    def peek(self):
+        if (self.first == None):
+            return ("Underflow")
+        else:
+            item = self.first.data
+            return item
 
+if __name__=='__main__':
+    queue = Queue()
+    queue.insert(1)
+    queue.insert(2)
+    queue.insert(3)
+    queue.insert(4)
+    queue.insert(5)
+    print (queue.remove())
+    print (queue.remove())
+    print (queue.remove())
+    print (queue.remove())
+    print (queue.remove())
+    print (queue.remove())
